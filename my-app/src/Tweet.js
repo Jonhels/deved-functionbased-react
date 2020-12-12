804360fe487f03d51d0141d91257ad3cbfe8d828
Jonhels <jonhels8888@gmail.com>
2020-12-12 09:38:26 +0100

    eslint

---
+++ my-app/src/Tweet.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+function Tweet(){
+    return(
+        <div>
+            <h3>Name</h3>
+            <p>This is a random tweet</p>
+            <h3>Number of likes</h3>
+        </div>
+    );
+}
+
+export default Tweet;
